3dac7a60713fe5d7eaad31e90a33ed8a011d2823
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2021-04-07 13:10:40 +0530

    hobbies added

---
+++ php/HConnect.php
@@ -0,0 +1,9 @@
+<?php
+class HConnect {
+    private $db = NULL;
+
+    const DB_SERVER = "localhost";
+    const DB_USER = "";
+    const DB_PASSWORD = "";
+    const DB_NAME = "hobby_connect";
+ ?>
